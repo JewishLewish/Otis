839fbd07d88dbabd7b81157b76a30f7f780187d9
JewishLewish <lanbrokhman@gmail.com>
2023-11-11 15:54:10 -0800

    Otis v1 Dataset 11/11/2023

--- training.py
+++ training.py
@@ -16,9 +16,7 @@ def main():
         encodings = tokenizer(text, padding="max_length", truncation=True, max_length=128)
 
         label = 0
-        print(row['label'])
         if row["label"] == 1: #spam
-            print("CONTAINS SPAM")
             label = 1
 
         encodings['label'] = label
@@ -73,7 +71,7 @@ def main():
 
     from transformers import TrainingArguments, Trainer
 
-    training_args = TrainingArguments(output_dir="./result", evaluation_strategy="epoch",num_train_epochs=5)
+    training_args = TrainingArguments(output_dir="./result", evaluation_strategy="epoch",num_train_epochs=10)
 
     trainer = Trainer(
         model=model,
@@ -86,13 +84,13 @@ def main():
     trainer.train()
     trainer.evaluate()
 
-    model.save_pretrained('./model/')
+    model.save_pretrained('./otisv1/')
 
     from transformers import AutoModelForSequenceClassification
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    new_model = AutoModelForSequenceClassification.from_pretrained('./model/').to(device)
+    new_model = AutoModelForSequenceClassification.from_pretrained('./otisv1/').to(device)
 
     from transformers import AutoTokenizer
 
@@ -126,37 +124,6 @@ def main():
     INPUT = """buy online and save viagra price for this high demand med best price for this high demand med best price for this high demand med buy nowbuy nowbuy price for this high demand med best price for this high demand med best price for this high demand med buy nowbuy nowbuy nowcialis soft price for this high demand med best price for this high demand med best price for this high demand med buy nowbuy nowbuy your penis width ( girth ) by 20 % gain up to 3 + full inches in length buy nowbuy now"""
 
     print(get_prediction(INPUT))
-
-def insert():
-    # Specify the path to your CSV file
-    csv_file_path = 'enron_spam_detector.csv'
-
-    # New row of data to be inserted
-    new_row = ['new_message_id', 'new_text', 'new_label', 'new_label_text', 'new_subject', 'new_message', 'new_date']
-
-    # Open the CSV file in read mode
-    with open(csv_file_path, 'r') as file:
-        # Create a CSV reader object
-        csv_reader = csv.reader(file)
-
-        # Read the existing data
-        existing_data = list(csv_reader)
-
-    # Insert the new row into the existing data
-    existing_data.append(new_row)
-
-    # Specify the path to the new CSV file (with the new row inserted)
-    new_csv_file_path = 'new_file_with_row.csv'
-
-    # Open the new CSV file in write mode
-    with open(new_csv_file_path, 'w', newline='') as new_file:
-        # Create a CSV writer object
-        csv_writer = csv.writer(new_file)
-
-        # Write the updated data to the new CSV file
-        csv_writer.writerows(existing_data)
-
-    print("CSV file with the new row has been created:", new_csv_file_path)
 
 if __name__ == "__main__":
     main()
